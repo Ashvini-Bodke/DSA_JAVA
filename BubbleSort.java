26bc9ea1b6ff0f2f8bfaef7aab99c9524134e656
Ashvini-Bodke <167331256+Ashvini-Bodke@users.noreply.github.com>
2024-07-15 22:56:07 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,37 @@
+// optimized bubble sort.
+public class BubbleSort {
+
+    public static void bubbleSort(int arr[]) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swap = false;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swap = true;
+
+                }
+            }
+            if (!swap) {
+                return;
+            }
+
+        }
+    }
+
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 2, 1, 3 };
+        bubbleSort(arr);
+        print(arr);
+    }
+}
