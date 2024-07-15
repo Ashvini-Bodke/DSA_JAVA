3b37a5effd821a6c993cced2bc7dda3d9c6b61a2
Ashvini-Bodke <167331256+Ashvini-Bodke@users.noreply.github.com>
2024-07-15 22:56:37 +0530

    Add files via upload

---
+++ CountingSort.java
@@ -0,0 +1,41 @@
+public class CountingSort {
+
+    public static void countingSort(int[] arr) {
+
+        // find max element in an array
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        // counting the frequency
+        int count[] = new int[largest + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        // sorting
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 1, 9, 7, 1, 2, 4 };
+        countingSort(arr);
+        print(arr);
+    }
+
+}
